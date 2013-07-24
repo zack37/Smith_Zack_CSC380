73dc138b39fe6b2805ebe2ea00574865818949a1
Zack Smith <zsmith@student.neumont.edu>
2013-07-23 18:44:59 -0600

    Made a servlet

---
+++ TakingARest/src/edu/neumont/csc380/RestRequest.java
@@ -0,0 +1,22 @@
+package edu.neumont.csc380;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * User: Zack Smith
+ * Date: 7/23/13
+ * Time: 6:02 PM
+ */
+public class RestRequest extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //request.get
+    }
+}
